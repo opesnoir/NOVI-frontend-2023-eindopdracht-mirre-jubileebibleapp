89551443f45a4e4cd8a5a49d139854ea7548951d
opesnoir <goodsurroundsme@outlook.com>
2023-01-23 09:36:18 +0100

    router routes opgezet, paginastructuur opgezet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import Nav from "./components/nav/Nav";
+import Footer from "./footer/Footer";
+import {Navigate, Route, Routes} from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import AboutBible from "./pages/aboutBible/AboutBible";
+import AboutJubilee from "./pages/aboutJubilee/AboutJubilee";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Profile from "./pages/profile/Profile";
+import Favorite from "./pages/favorite/Favorite";
+import NotFound from "./pages/notFound/NotFound";
 
 function App() {
-  return (
+
+    const auth = false;
+
+    return (
     <>
-      <h1>Codeer hier</h1>
+      <Nav/>
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/about" element={<About/>}></Route>
+            <Route path="/about-Bible" element={<AboutBible/>}></Route>
+            <Route path="/about-Jubilee" element={<AboutJubilee/>}></Route>
+            <Route path="/signup" element={<Register/>}></Route>
+            <Route path="/signin" element={<Login/>}></Route>
+            <Route path="/favorite" element={<Favorite/>}></Route>
+            <Route path="/profile" element={auth === true ? <Profile/> : <Navigate to="/signup"/>}></Route>
+            <Route path="/favorite-auth" element={auth === true ? <Favorite/> : <Navigate to="/search"/>}></Route>
+            <Route path="*" element={<NotFound/>}></Route>
+        </Routes>
+        <Footer/>
     </>
   );
 }
