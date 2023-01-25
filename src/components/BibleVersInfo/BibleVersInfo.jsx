810750afcc2e443824399aeb4aba61a868b86312
opesnoir <goodsurroundsme@outlook.com>
2023-01-25 11:51:37 +0100

    homepagina dagelijks Bijbelvers api opgezet en als component toegevoegd. Daarnaast blok naar over de Bijbel toegevoegd, inclusief link to.

---
+++ src/components/BibleVersInfo/BibleVersInfo.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Wrapper from "../Wrapper/Wrapper";
+import {Link} from "react-router-dom";
+import Button from "../Button/Button";
+import granaatappel from "../../assets/homepage-pomegranate-pexels-pixabay-65256 copy.jpg"
+import DailyVers from "../DailyVers/DailyVers";
+
+
+const BibleVersInfo = () => {
+
+    return (
+        <>
+            <div>
+                <div>
+                    <img src={granaatappel} alt="Afbeelding van een granaatappel"/>
+                    <div>
+                        <h2>Bijbeltekst van de dag</h2>
+                        <p><DailyVers/></p>
+                    </div>
+                    <div>
+                        <h2>De Bijbel</h2>
+                        <p>Achtergrondinformatie</p>
+                        <p>De Bijbel is een verzameling van zesenzestig boeken...</p>
+                        <Link to="/about-Bible">
+                            <Button
+                                type="button"
+                                name="Lees meer"
+                            />
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default BibleVersInfo;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import SectionInput from "../../components/SectionInput/SectionInput";
 import Wrapper from "../../components/Wrapper/Wrapper";
 import VideoBox from "../../components/VideoBox/VideoBox";
+import BibleVersInfo from "../../components/BibleVersInfo/BibleVersInfo";
+
+
 
 const Home = () => {
     return (
@@ -12,6 +15,7 @@ const Home = () => {
                 paragraphOne="Welkom op Jubilee BibleApp. Zoek hieronder een Bijbelvers op titel of steekwoord in ruim 40 vertalingen. Selecteer een Bijbel, voer een zoekterm in en druk op de button om de resultaten te bekijken."
                 />
             </Wrapper>
+            <BibleVersInfo/>
             <Wrapper>
                 <VideoBox
                     width="359"
