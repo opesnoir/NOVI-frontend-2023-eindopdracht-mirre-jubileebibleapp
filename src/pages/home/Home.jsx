e1917b7440a46207425fa13f4e44498a731a2907
opesnoir <goodsurroundsme@outlook.com>
2023-01-26 14:30:16 +0100

    video sectie parent toegevoegd om de displayflex aan te passen, de youtubevideos zijn nu goed gepositioneerd

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -40,27 +40,29 @@ const Home = () => {
                     </Wrapper>
                     <BibleVersInfo/>
                     <Wrapper>
-                        <VideoCard
-                            width="359"
-                            height="206"
-                            src="https://www.youtube.com/embed/XEjDiMjkVRo"
-                            title="What is the Bible"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        />
-                        <VideoCard
-                            width="359"
-                            height="206"
-                            src="https://www.youtube.com/embed/7_CGP-12AE0"
-                            title="Story of the Bible"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        />
-                        <VideoCard
-                            width="359"
-                            height="206"
-                            src="https://www.youtube.com/embed/takEeHtRrMw"
-                            title="The New Humanity"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        />
+                        <div className={styles.card__parent}>
+                            <VideoCard
+                                width="359"
+                                height="206"
+                                src="https://www.youtube.com/embed/XEjDiMjkVRo"
+                                title="What is the Bible"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            />
+                            <VideoCard
+                                width="359"
+                                height="206"
+                                src="https://www.youtube.com/embed/7_CGP-12AE0"
+                                title="Story of the Bible"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            />
+                            <VideoCard
+                                width="359"
+                                height="206"
+                                src="https://www.youtube.com/embed/takEeHtRrMw"
+                                title="The New Humanity"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            />
+                        </div>
                     </Wrapper>
                     <Wrapper>
                         <Tile
