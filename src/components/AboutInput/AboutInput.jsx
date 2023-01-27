dc96fd7bcb1d43ac55f349e1a56b2e81d200d15a
opesnoir <goodsurroundsme@outlook.com>
2023-01-27 17:56:34 +0100

    tekst about van styling voorzien, ik vind de donkere kleur mooier, maar ik twijfel of de lichtere kleur beter past bij het geheel, afbeelding nog van styling voorzien.

--- src/components/AboutInput/AboutInput.jsx
+++ src/components/AboutInput/AboutInput.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import styles from "../SectionInput/SectionInput.module.css";
+import styles from "./AboutInput.module.css";
 
 const AboutInput = ({title, paragraphOne, paragraphTwo, children, signature}) => {
     return (
         <>
-            <article>
-                <h1 className={styles.about__h1}>{title}</h1>
-                <p className={styles.about__p}>{paragraphOne}</p>
-                <p className={styles.about__p}>{paragraphTwo}</p>
-                {children}
-                <p className={styles.about__signature}>{signature}</p>
-            </article>
+            <section>
+                <article className={styles.about__container__outer}>
+                    <div className={styles.about__container__inner}>
+                    <h1 className={styles.about__h1}>{title}</h1>
+                    <p className={styles.about__p}>{paragraphOne}</p>
+                    <p className={styles.about__p}>{paragraphTwo}</p>
+                    {children}
+                    <p className={styles.about__signature}>{signature}</p>
+                    </div>
+                </article>
+            </section>
         </>
     );
 };
