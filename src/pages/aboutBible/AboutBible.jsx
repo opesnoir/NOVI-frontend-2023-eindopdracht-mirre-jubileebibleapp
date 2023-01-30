ff6218331fae3b22e351c44eb53e49860a26687f
opesnoir <goodsurroundsme@outlook.com>
2023-01-30 09:58:06 +0100

    over de Bijbel pagina aangepast, zowel qua inhoud als qua volgorde

--- src/pages/aboutBible/AboutBible.jsx
+++ src/pages/aboutBible/AboutBible.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Image from "../../components/Image/Image";
 import AboutInput from "../../components/AboutInput/AboutInput";
-import styles from "./AboutBible.module.css";
+/*import styles from "./AboutBible.module.css";*/
+import styles from "../../components/AboutInput/AboutInput.module.css";
 import schapen from "../../assets/about-sheeps-pexels-nazım-can-pirinç-11443337.jpg"
 
 const AboutBible = () => {
@@ -9,12 +10,11 @@ const AboutBible = () => {
         <>
             <AboutInput
                 title="Over de Bijbel"
-                paragraphOne="De Bijbel is een verzameling van zesenzestig boeken. Het zijn geen losse boeken, maar afgeronde Bijbelgedeelten die ieder een stuk van een groter verhaal vertellen. Te weten het verhaal van de Elohim JHWH (de God van Abraham, Isaak en Jacob) en Zijn plan om de gevallen schepping te redden van de gevolgen van de zondeval."
-                paragraphTwo=" “Heel de Schrift is door God ingegeven en is nuttig om daarmee te onderwijzen, te weerleggen, te verbeteren en op te voeden in de rechtvaardigheid, opdat de mens die God toebehoort, volmaakt zou zijn, tot elk goed werk volkomen toegerust.” - 2 Timoteüs 3:16 en 17 "
-                children=""
+                paragraphOne=" “Heel de Schrift is door God ingegeven en is nuttig om daarmee te onderwijzen, te weerleggen, te verbeteren en op te voeden in de rechtvaardigheid, opdat de mens die God toebehoort, volmaakt zou zijn, tot elk goed werk volkomen toegerust.” - 2 Timoteüs 3:16 en 17 "
+                paragraphTwo="De Bijbel is een verzameling van zesenzestig boeken. Het zijn geen losse boeken, maar afgeronde Bijbelgedeelten die ieder een stuk van een groter verhaal vertellen. Te weten het verhaal van de Elohim JHWH (de God van Abraham, Isaak en Jacob) en Zijn plan om de gevallen schepping te redden van de gevolgen van de zondeval."
+                signature="Jubilee"
             >
-                <p className={styles.about__p}>Meer leren over de Bijbel. Neem dan een kijkje op de website van The Bible Project. The Bible Project is een non-profit organisatie die geanimeerde video's en studiematerialen aanbiedt, om de Bijbel toegankelijk te maken voor iedereen. Naar de website van The Bible Project? Klik dan: </p>
-                <p><a className={styles.about__a} target="_blank" href="https://bibleproject.com/Dutch/">hier.</a></p>
+                <p className={styles.about__p}>De Bijbel werd geschreven door ongeveer 40 auteurs met verschillende achtergronden, over een periode van 1500 jaar. Hieronder bevonden zich profeten, priesters, belastingverzamelaars, vissers, tentmakers en herders. Elke auteur biedt een uniek perspectief, maar allen verkondigen de ene ware God en de redding door Jezus Christus. Ondanks het feit dat de Bijbel over een lange tijd geschreven is, zijn er geen tegenstrijdigheden en fouten in de traditionele manuscripten afkomstig uit Antioch. Deze 5210 manuscripten vormen de basis voor onder andere de King James Bible uit 1611 (‘The Holy Bible: 1611 Edition, King James Version’).</p>
             </AboutInput>
             <Image
                 image={schapen}
