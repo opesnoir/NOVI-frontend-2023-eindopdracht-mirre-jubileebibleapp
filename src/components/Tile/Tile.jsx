2555adf4a7fbd740e38d0cf0801a3f7b6e2f427a
opesnoir <goodsurroundsme@outlook.com>
2023-02-01 15:48:48 +0100

    media querie toegevoegd aan homepage

--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -4,13 +4,13 @@ import styles from "./Tile.module.css";
 const Tile = ({title, paragraph, children}) => {
     return (
         <>
-            <div>
-                <div className={styles.tile__container__inner}>
-                    <h2 className={styles.tile__h}>{title}</h2>
-                    <p className={styles.tile__p}>{paragraph}</p>
-                    {children}
+                <div>
+                    <div className={styles.tile__container__inner}>
+                        <h2 className={styles.tile__h}>{title}</h2>
+                        <p className={styles.tile__p}>{paragraph}</p>
+                        {children}
+                    </div>
                 </div>
-            </div>
         </>
     );
 };
